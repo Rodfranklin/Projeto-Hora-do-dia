f5dfe9686df4059a1aadc48038aed1dcbd2fff38
RODRIGO  FRANKLIN <118393038+Rodfranklin@users.noreply.github.com>
2023-04-11 00:38:52 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+function carregar () { 
+    var msg = window.document.getElementById('msg')
+var img = window.document.getElementById('imagem')
+var data = new Date ()
+var hora = data.getHours()
+var hora = 01
+msg.innerHTML= `agora são ${hora} horas.`
+
+if ( hora>=0 && hora < 12) {//BOM DIA (comentario em js)
+    img.src = 'imagens/manha.png'
+document.body.style.background= '#e2cd9f'} 
+
+   
+
+    else if 
+    ( hora >=12 && hora <18)  {  //(BOA TARDE)
+        img.src = 'imagens/tarde.png'
+    document.body.style.background = '#b9846f'}
+    
+
+        else 
+        {img.src
+       //(BOA NOITE)
+       img.src = 'imagens/night.png'
+       document.body.style.background = '#515154'}}
+    
+
+
+
+
+
